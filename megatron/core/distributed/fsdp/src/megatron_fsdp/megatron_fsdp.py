Fix gradient double buffer enforcement guard.
Signed-off-by: Cory Ye <cye@nvidia.com>

megatron/core/distributed/fsdp/src/megatron_fsdp/megatron_fsdp.py
@@ -337,11 +337,6 @@ class MegatronFSDP(torch.nn.Module):
                 # when the parameter is created.
                 param.__fsdp_param__ = True
 
-        # Attach MegatronFSDP reference to the parameter for API access.
-        # Will be inherited by the distributed parameter.
-        for param in self.module.parameters():
-            setattr(param, "_megatron_fsdp_model", self)
-
         self._init_fsdp_param_and_grad_buffer()
         self._register_fsdp_hooks(self.module)
         self.microbatch_count = 0
@@ -602,12 +597,7 @@ class MegatronFSDP(torch.nn.Module):
                 # If TransformerEngine gradient accumulation is fused, then param.get_main_grad()
                 # already holds the wgrad and param.grad_added_to_main_grad=True.
                 if not param.grad_added_to_main_grad:
-                    # Get `main_grad` will allocate bucket, check that the currently
-                    # used main_grad buffer does not exceed the scope of two FSDP Unit
-                    # Modules, i.e., the buffer limit imposed by double-buffer allocator.
-                    if self.ddp_config.fsdp_double_buffer:
-                        self.grad_reduce_pipeline._enforce_double_buffer_limit([group_id])
-
+                    # Allocate a unsharded gradient buffer.
                     param.main_grad = param.get_main_grad()
                     if param.grad is not None:
                         if self.report_nan_in_param_grad:
@@ -1357,6 +1347,8 @@ class MegatronFSDP(torch.nn.Module):
             # DTensor parameter is managed by Megatron FSDP.
             if not hasattr(dist_param, "__fsdp_param__"):
                 dist_param.__fsdp_param__ = True
+            if not hasattr(dist_param, "_megatron_fsdp_model"):
+                dist_param._megatron_fsdp_model = self
             _replace_module_parameter(self.module, name, dist_param)
 
         # Handle shared weights
@@ -1369,6 +1361,8 @@ class MegatronFSDP(torch.nn.Module):
 
         for name, _ in self.module.named_parameters():
             assert name in self.raw_param, f"Raw parameter {name} not found in module."
+            if not hasattr(self.raw_param[name], "_megatron_fsdp_model"):
+                self.raw_param[name]._megatron_fsdp_model = self
             _replace_module_parameter(self.module, name, self.raw_param[name])
 
         # Handle shared weights